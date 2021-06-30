f0f312da03f1fe4db9f5fda66e8ffecea0414bae
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-06-29 20:05:21 -0400

    Completed assignment

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,5 +1,27 @@
+require 'pry'
+
 def find_min_in_nested_arrays(src)
   # src will be an array of arrays of integers
   # Produce a new Array that contains the smallest number of each of the nested arrays
+
+  outer_results = []
+  row_index = 0
+  while row_index < src.count do
+    element_index = 0
+    min_temp = src[row_index][0]
+    # binding.pry
+
+    while element_index < src[row_index].count do
+      if src[row_index][element_index] < min_temp
+        min_temp = src[row_index][element_index]
+      end
+      element_index += 1
+    end
+  
+    outer_results << min_temp
+    row_index += 1
+  end
+  
+  outer_results
 
 end
